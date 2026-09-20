73cb79db78f407cb026ae922051c5efa7ccf8c64
Mxxjxe <minjoo030226@gmail.com>
2026-09-20 20:43:15 +0900

    S04: 다이아몬드 하단 삼각형 4개 추가 및 완성

--- Assets/S04/S04_CustomDiamond.cs
+++ Assets/S04/S04_CustomDiamond.cs
@@ -20,7 +20,13 @@ public class S04_CustomDiamond : MonoBehaviour
             0, 1, 4,
             1, 2, 4,
             2, 3, 4,
-            3, 0, 4
+            3, 0, 4,
+
+
+            1, 0, 5,
+            2, 1, 5,
+            3, 2, 5,
+            0, 3, 5
         };
 
         Mesh mesh = new Mesh();

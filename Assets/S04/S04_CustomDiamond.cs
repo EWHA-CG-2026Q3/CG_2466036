64d583c7a1a4986294d58002fbfdf0cf229734cb
Mxxjxe <minjoo030226@gmail.com>
2026-09-20 20:36:05 +0900

    기본 정점 설정

---
+++ Assets/S04/S04_CustomDiamond.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S04_CustomDiamond : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 0f, 0f),       // v0 
+            new Vector3(0f, 0f, 1f),       // v1 
+            new Vector3(1f, 0f, 1f),       // v2 
+            new Vector3(1f, 0f, 0f),       // v3 
+            new Vector3(0.5f, 1f, 0.5f),   // v4 
+            new Vector3(0.5f, -1f, 0.5f),  // v5 
+        };
+
+        int[] triangles = new int[]
+        {
+            
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
